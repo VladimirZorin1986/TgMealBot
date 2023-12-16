b736a1d0e745455c743577e26235c110966f6471
Vladimir1986 <pls.github@gmail.com>
2023-12-16 20:25:55 +0300

    refactor: set manager support to user handlers

--- handlers/new_user_handlers.py
+++ handlers/new_user_handlers.py
@@ -9,8 +9,6 @@ from keyboards.inline import show_canteens_kb, show_places_kb_new
 from states.user_states import AuthState, ChangeDeliveryPlace
 from services.other_services import (terminate_state_branch, set_track_callback,
                                      update_track_callback, get_track_callback)
-from services.user_services import update_customer_data
-from handlers.functions import process_choice_canteens_or_places, process_choice_delivery_place
 from middlewares.local_middlewares import ServiceManagerMiddleware
 from services.managers import UserManager
 from presentation.responses import message_response, callback_response, edit_response
@@ -122,15 +120,43 @@ async def process_get_place(
 
 
 @router.message(F.text.endswith('Изменить место доставки'), StateFilter(default_state))
-async def process_change_delivery_place(message: Message, session: AsyncSession, state: FSMContext):
-    await process_choice_canteens_or_places(
-        message, state, session, ChangeDeliveryPlace.set_new_canteen, ChangeDeliveryPlace.set_new_place
-    )
+async def process_change_delivery_place(
+        message: Message, session: AsyncSession, state: FSMContext, manager: UserManager):
+    text = 'Произошла ошибка при обработке контактных данных.'
+    reply_markup = None
+    try:
+        await manager.validate_customer(session, message, state)
+        canteens = await manager.receive_canteens(session)
+        if manager.allowed_several_canteens():
+            await state.set_state(ChangeDeliveryPlace.set_new_canteen)
+            text = 'Выберите столовую, в которой будете заказывать еду:'
+            reply_markup = show_canteens_kb(canteens)
+        else:
+            await state.set_state(ChangeDeliveryPlace.set_new_place)
+            text = 'Выберите место доставки:'
+            reply_markup = show_places_kb_new(await manager.receive_canteen_places(canteen=canteens[0]))
+
+    except (IsNotCustomer, ValidCanteensNotExist) as exc:
+        await state.clear()
+        reply_markup = ReplyKeyboardRemove()
+        if isinstance(exc, IsNotCustomer):
+            text = 'Вас нет в списке заказчиков столовой.'
+        else:
+            text = 'У вас истек срок разрешения для заказа еды.'
+    finally:
+        await message_response(
+            message=message,
+            text=text,
+            reply_markup=reply_markup,
+            state=state,
+            delete_after=True
+        )
 
 
 @router.message(F.text.endswith('Изменить место доставки'), ~StateFilter(default_state))
-async def process_menu_button_with_context(message: Message, session: AsyncSession, state: FSMContext):
-    await message.answer(
+async def process_menu_button_with_context(message: Message):
+    await message_response(
+        message=message,
         text='Сначала закончите выполняемое действие. '
              'Для отмены действия нажмите на команду /cancel',
         reply_markup=ReplyKeyboardRemove()
@@ -139,20 +165,44 @@ async def process_menu_button_with_context(message: Message, session: AsyncSessi
 
 @router.callback_query(StateFilter(ChangeDeliveryPlace.set_new_place), F.data.startswith('canteen'))
 @router.callback_query(StateFilter(ChangeDeliveryPlace.set_new_canteen))
-async def process_change_place(callback: CallbackQuery, state: FSMContext, session: AsyncSession):
-    await process_choice_delivery_place(
-        callback, state, session, ChangeDeliveryPlace.set_new_canteen, ChangeDeliveryPlace.set_new_place
-    )
+async def process_change_place(
+        callback: CallbackQuery, state: FSMContext, session: AsyncSession, manager: UserManager):
+    markup = show_places_kb_new(await manager.receive_canteen_places(session=session, callback=callback))
+    current_state = await state.get_state()
+    message_text = 'Выберите новое место доставки еды:'
+    if current_state == ChangeDeliveryPlace.set_new_canteen:
+        await state.set_state(ChangeDeliveryPlace.set_new_place)
+        msg = await message_response(
+            message=callback.message,
+            text=message_text,
+            reply_markup=markup,
+            state=state,
+            return_msg=True
+        )
+        await set_track_callback(callback, msg, state)
+    else:
+        track_cb = await get_track_callback(state)
+        if track_cb.callback_data != callback.data:
+            await edit_response(
+                message=track_cb.message,
+                text=message_text,
+                reply_markup=markup
+            )
+            await update_track_callback(track_cb, callback, state)
+    await callback_response(callback)
 
 
 @router.callback_query(StateFilter(ChangeDeliveryPlace.set_new_place), F.data.startswith('place'))
-async def process_get_place(callback: CallbackQuery, state: FSMContext, session: AsyncSession):
-    await update_customer_data(session, callback, state)
-    await callback.message.answer(
+async def process_get_place(
+        callback: CallbackQuery, state: FSMContext, session: AsyncSession, manager: UserManager):
+    await manager.change_delivery_place(session, callback)
+    await message_response(
+        message=callback.message,
         text='Теперь вы можете сделать заказ.',
         reply_markup=initial_kb()
     )
-    await callback.answer(
+    await callback_response(
+        callback=callback,
         text='Место доставки успешно изменено!',
         show_alert=True
     )
