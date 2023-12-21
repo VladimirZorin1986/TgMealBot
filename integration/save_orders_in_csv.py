8b2bd64995ad443ba7770f6928633cad91d6a378
Vladimir1986 <pls.github@gmail.com>
2023-12-21 20:39:28 +0300

    relocate integration files

--- integration/save_orders_in_csv.py
+++ integration/save_orders_in_csv.py
@@ -1,10 +1,7 @@
 import csv
 import asyncio
-import datetime
-from decimal import Decimal
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import select, text
-from database.models import Order, OrderDetail, Menu, MenuPosition
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import text
 
 
 async def async_main():
@@ -14,15 +11,18 @@ async def async_main():
     )
 
     async with engine.connect() as conn:
-        stmt = select(
-            Order.id,
-            Menu.canteen_id,
-            Order.menu_id,
-            Menu.date,
-            Order.customer_id,
-            Order.place_id,
-            Order.amt
-        ).join(Menu)
+        stmt = text('''
+           SELECT mo.id,
+                  menu.canteen_id,
+                  mo.menu_id,
+                  menu.date,
+                  mo.customer_id,
+                  mo.place_id,
+                  mo.amt
+           FROM meal_order mo JOIN menu ON (mo.menu_id=menu.id)
+           WHERE mo.sent_to_eis IS NULL
+             AND menu.end_time::date = now()::date
+        ''')
         result = await conn.execute(stmt)
         with open('static/orders.csv', 'w', newline='') as csvfile:
             fieldnames = ['id', 'canteen_id', 'menu_id', 'date', 'customer_id', 'place_id', 'amt']
@@ -31,12 +31,15 @@ async def async_main():
             for order in result.all():
                 writer.writerow(dict(zip(fieldnames, order)))
 
-        stmt = text('''SELECT od.id,
-       od.order_id,
-       ROW_NUMBER() OVER (PARTITION BY od.order_id, mp.menu_id ORDER BY mp.id) AS num_of_menu,
-       od.quantity,
-       od.quantity * mp.cost AS amt
-FROM order_detail od join menu_position mp ON (od.menu_pos_id=mp.id)''')
+        stmt = text('''
+           SELECT od.id,
+                  od.order_id,
+                  ROW_NUMBER() OVER (PARTITION BY od.order_id, mp.menu_id ORDER BY mp.id) AS num_of_menu,
+                  od.quantity,
+                  od.quantity * mp.cost AS amt
+           FROM order_detail od JOIN menu_position mp ON (od.menu_pos_id=mp.id)
+           WHERE mo.order_id = 29
+        ''')
         result2 = await conn.execute(stmt)
         with open('static/details.csv', 'w', newline='') as csvfile:
             fieldnames = ['id', 'order_id', 'num_of_menu', 'qty', 'amt']
